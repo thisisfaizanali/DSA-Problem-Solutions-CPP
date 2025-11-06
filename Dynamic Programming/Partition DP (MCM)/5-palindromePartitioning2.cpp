bfc44a25361481324a652f2466538a15cbdb5fa1
thisisfaizanali <faizanaliwhb@gmail.com>
2025-11-06 16:48:07 +0530

    feat : add solution using Front Partition DP

---
+++ Dynamic Programming/Partition DP (MCM)/5-palindromePartitioning2.cpp
@@ -0,0 +1,123 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++  Bismillah  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Memoization :
+class Solution
+{
+private:
+    int solve(int i, string &s, vector<int> &dp)
+    {
+        // Base case: if we reach end of string, no cuts/partitions left
+        if (i == s.size())
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int mini = 1e9;   // initialize minimum partitions to large number
+        string temp = ""; // to build substring s[i..j]
+
+        // Try every possible end index j from i..end
+        for (int j = i; j < s.size(); ++j)
+        {
+            temp += s[j]; // expand current substring by including s[j]
+
+            // if current substring s[i..j] is palindrome
+            if (isPalindrome(temp))
+            {
+                // 1 + solve(j + 1, s, dp):
+                // '1' → current palindromic block (partition) made here
+                // 'solve(j + 1, s, dp)' → solve remaining substring after j
+                // together count total partitions for s[i..n-1]
+                int cost = 1 + solve(j + 1, s, dp);
+
+                mini = min(mini, cost);
+            }
+        }
+
+        return dp[i] = mini;
+    }
+
+private:
+    bool isPalindrome(string &s)
+    {
+        int l = 0, r = s.size() - 1;
+        while (l < r)
+        {
+            if (s[l++] != s[r--])
+                return false;
+        }
+        return true;
+    }
+
+public:
+    int minCut(string &s)
+    {
+        int n = s.size();
+        vector<int> dp(n, -1); // dp[i] stores min partitions from i..end
+
+        // solve(0) gives number of palindromic partitions
+        // subtract 1 because cuts = partitions - 1
+        int ans = solve(0, s, dp) - 1;
+
+        // return minimum number of cuts
+        return ans;
+    }
+};
+
+// Tabulation :
+class Solution
+{
+private:
+    bool isPalindrome(string &s)
+    {
+        int l = 0, r = s.size() - 1;
+        while (l < r)
+        {
+            if (s[l++] != s[r--])
+                return false;
+        }
+        return true;
+    }
+
+public:
+    int minCut(string &s)
+    {
+        int n = s.size();
+
+        // dp[i] = min partitions needed for substring s[i..end]
+        // initialize with large values
+        vector<int> dp(n + 1, 1e9);
+
+        // base case: dp[n] = 0 (no partitions needed after end)
+        dp[n] = 0;
+
+        // fill dp from end to start (bottom-up)
+        for (int i = n - 1; i >= 0; --i)
+        {
+            string temp = ""; // build substring s[i..j]
+
+            // try all possible end positions j
+            for (int j = i; j < s.size(); ++j)
+            {
+                temp += s[j]; // add character to substring
+
+                // if substring s[i..j] is palindrome
+                if (isPalindrome(temp))
+                {
+                    // 1 + dp[j + 1]:
+                    // '1' = one palindromic block formed here
+                    // dp[j + 1] = partitions for remaining substring
+                    int cost = 1 + dp[j + 1];
+
+                    dp[i] = min(cost, dp[i]);
+                }
+            }
+        }
+
+        // dp[0] = total partitions for whole string
+        // cuts = partitions - 1
+        return dp[0] - 1;
+    }
+};
